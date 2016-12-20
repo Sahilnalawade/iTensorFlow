4436ffd0ccb1a7bcd60169090e814c6dbbc75e26
stnava <stnava@gmail.com>
2016-12-20 17:45:59 -0500

    DOC: simple itk option

--- src/r2python/readArray.py
+++ src/r2python/readArray.py
@@ -1,6 +1,6 @@
 import numpy as np
 n = 3
-filename = '/tmp/tenbytenbytenfloat.raw'
+filename = '/tmp/kbykbykfloat.raw'
 with open(filename, 'rb') as f:
   data = np.fromfile(f, dtype=np.float32)
   array = np.reshape(data, [n, n, n])
@@ -10,3 +10,32 @@ with open(filename, 'rb') as f:
 # now try to read a RcppCNPy object
 m = np.load("/tmp/randmat.npy")
 m = np.reshape( np.load("/tmp/randmat3.npy"), [n, n, n])
+
+# now a nifti multi-channel with nibabel
+# see http://nipy.org/nibabel/gettingstarted.html
+import os
+import numpy as np
+import nibabel as nib
+img = nib.load( '/tmp/kbykbykfloat.nii.gz' )
+data = img.get_data()
+mimg = nib.load( '/tmp/kbykbykfloatmc.nii.gz' )
+mata = mimg.get_data()
+
+
+# now h5
+from __future__ import print_function
+import numpy as np
+import h5py
+fn = '/tmp/myData.h5'
+with h5py.File( fn, 'r' ) as hf:
+    print('List of arrays in this file: \n', hf.keys())
+    data = hf.get('thdarr')
+    np_data = np.array(data)
+    print('Shape of the array dataset_1: \n', np_data.shape)
+
+
+# now try the multi-channel image read with simpleitk
+from skimage import io
+import SimpleITK as sitk
+fn = '/tmp/kbykbykfloatmc.nii.gz'
+myimg = io.imread( fn, plugin='simpleitk').astype(float)
--- src/r2python/writeArray.R
+++ src/r2python/writeArray.R
@@ -1,7 +1,7 @@
 library( ANTsR )
 n = 3
 thdimg = makeImage( rep(n,3), rnorm( n^3 ) )
-antsImageWrite( thdimg, '/tmp/tenbytenbytenfloat.mhd' )
+antsImageWrite( thdimg, '/tmp/kbykbykfloat.mhd' )
 
 
 library( RcppCNPy )
@@ -9,3 +9,20 @@ thdarr = as.array( thdimg )
 npySave( "/tmp/randmat.npy", thdarr[,,2] )
 # this will fail to write an array but will write a numeric vector that can be reshaped
 npySave( "/tmp/randmat3.npy", thdarr )
+# but the above does not have the correct shape ...
+
+# try nifti and nibable
+antsImageWrite( thdimg, '/tmp/kbykbykfloat.nii.gz' )
+
+
+# save a multi-channel image and see if python can read it (via nibabel)
+mimg = mergeChannels( list( thdimg, thdimg ) )
+antsImageWrite( mimg, '/tmp/kbykbykfloatmc.nii.gz' )
+
+
+# try hdf5
+library( rhdf5 )
+fn = '/tmp/myData.h5'
+h5createFile( fn )
+h5write( thdarr, file = fn , 'thdarr')
+h5ls( fn )
