c370fc7c9be816012d431c737e4a8deb27f4fda6
stnava <stnava@gmail.com>
2017-01-10 17:20:04 -0500

    ENH: move some core code to antsrnpy

--- src/R/buildConstantHeaderSyntheticDataNumpy.R
+++ src/R/buildConstantHeaderSyntheticDataNumpy.R
@@ -9,32 +9,20 @@ n = c(1000,200)
 mydim = 2
 idim = rep( 32, mydim )
 odir = paste( "data/dim", length(idim), "D/regression/spheresRad/", c("train/singlechannel","test/singlechannel"),"/", sep='' )
-img = makeImage( idim, voxval = 0,
-  spacing = rep(1, length(idim)) )
-msk = img + 1
-spat = imageDomainToSpatialMatrix( msk, msk )
-spatx = makeImage( dim(msk), spat[,1] )
-spaty = makeImage( dim(msk), spat[,2] )
-spatlist = list( spatx,spaty )
+img = makeImage( idim, voxval = 0, spacing = rep(1, length(idim)) )
 baserad = 3
 for ( ct in 1:2 ) {
   myct = 1
   comdf = data.frame( x=0, y=0, r1=0, r2=0 )
   for ( k in 1000:(1000+n[ct]-1) ) {
-    pts = matrix( nrow = 1, ncol = mydim )
-    ctr = idim / 2
-    pts[1, ] = antsTransformIndexToPhysicalPoint( img, ctr ) +
-      rnorm( length(idim), 0, 3 )
-    r1 = rnorm(1,baserad,1)
-    r2 = rnorm(1,baserad,1)
-    ptsi = makePointsImage( pts, msk, radius = r1 )
-    ptsi = ptsi + makePointsImage( pts, msk, radius = r2  )
-    comdf[ myct,  ] = c( getCenterOfMass( ptsi ), r1, r2 )
-    ptsi[ msk == 1 ] = ptsi[ msk == 1 ] + rnorm( sum(msk==1),  0, 0.1 )
+    r1 = rnorm( 1, baserad, 1 )
+    r2 = rnorm( 1, baserad, 1 )
+    sim = simulateSphereData( img, radius = c( r1, r2 ), positionNoiseLevel = c( 0, 3 ) )
+    comdf[ myct,  ] = c( sim$centerOfMass, r1, r2 )
     # antsr framework
     ofn = paste( odir[ct], "/", sep='' )
     dir.create( ofn, showWarnings = FALSE, recursive = TRUE, mode = "0777")
-    ptsj = antsImageClone( iMath( ptsi, "Normalize") * 255, "unsigned char" )
+    ptsj = antsImageClone( iMath( sim$image, "Normalize") * 255, "unsigned char" )
     ofn = paste( odir[ct], "/sphere", k, ".npy", sep='' )
     writeANTsImageToNumpy( ptsi, ofn )
 #    antsImageWrite( ptsj, ofn )
